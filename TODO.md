57967864d92fb517c055e55319914572655fa297
Dae Houlihan <dev@daeh.info>
2025-06-23 18:55:39 -0400

    Release v0.0.5
    Clean public release snapshot

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODO
+
+- more vertical padding between sections in prefs pane
+- ######
+- add download counts from arxiv?
+- ######
+- coordinate tsconfig files
+- hidden prefs for text colors
+- (remove status for autoupdate at launch?)
+- (make autoupdate callable from tools menu)
---
+++ src/addon.ts
@@ -0,0 +1,42 @@
+import { config } from '../package.json'
+
+import hooks from './hooks'
+import { createZToolkit } from './utils/ztoolkit'
+
+import type { ColumnOptions, DialogHelper } from 'zotero-plugin-toolkit'
+
+class Addon {
+  public data: {
+    alive: boolean
+    config: typeof config
+    // Env type, see build.js
+    env: 'development' | 'production'
+    ztoolkit: ZToolkit
+    locale?: {
+      current: any
+    }
+    prefs?: {
+      window: Window
+      columns?: ColumnOptions[]
+      rows?: Record<string, string>[]
+    }
+    dialog?: DialogHelper
+  }
+  // Lifecycle hooks
+  public hooks: typeof hooks
+  // APIs
+  public api: object
+
+  constructor() {
+    this.data = {
+      alive: true,
+      config,
+      env: __env__,
+      ztoolkit: createZToolkit(),
+    }
+    this.hooks = hooks
+    this.api = {}
+  }
+}
+
+export default Addon
