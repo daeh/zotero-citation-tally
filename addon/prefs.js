87f832ecd2c5f72282a40bd71205d1d65bd47d71
Dae Houlihan <dev@daeh.info>
2025-08-20 23:26:47 -0400

    Release v0.0.7
    Clean public release snapshot

--- addon/prefs.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-pref('databaseOrderExposed', 'crossref,semanticscholar')
-pref('databaseOrder', 'crossref,semanticscholar')
-pref('autoUpdate', 'never')
-pref('autoUpdateCutoff', '6')
-pref('useColors', 'color')
-pref('ignoredItems', '{}')
-pref('rateLimits', '{"crossref":1000,"semanticscholar":3000,"inspire":1000}')
